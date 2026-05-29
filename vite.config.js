2350a9e54c32c3dab735f860c30dd4cdb263a464
Noah Hanford (spaced) <spaced@csh.rit.edu>
2026-05-29 15:37:15 -0400

    vite docs work

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import { version } from './package.json'
 
 export default defineConfig({
+  base: './',
   define: {
-    'import.meta.env.VITE_APP_VERSION': version,
+    'import.meta.env.VITE_APP_VERSION': JSON.stringify(version),
+  },
+  build: {
+    outDir: "s3-output"
   }
 })
